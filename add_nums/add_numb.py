1726214ad32a8a1302d0c92525fd01532955d5a9
Elise Novitski <elisenovitski@gmail.com>
2013-06-24 13:43:12 -0400

    "Replaced add_two with add_three"

--- add_nums/add_numb.py
+++ add_nums/add_numb.py
@@ -1,8 +1,8 @@
-def add_two(a,b):
-	return a+b
+def add_three(a,b,c):
+	return a+b+c
 
 def main():
-	print ("2+3=", addtwo(2,3))
+	print ("2+3+4=", addthree(2,3,4))
 
 if __name__== "__main__"
 	main()
